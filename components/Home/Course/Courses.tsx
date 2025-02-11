9e4d5920e6d297fb853ec32adca96cbb41f8db2e
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-11 23:57:40 +0530

    Create Courses.tsx

---
+++ components/Home/Course/Courses.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image';
+import { coursesData } from '@/data/data';
+import CourseCard from './CourseCard';
+
+const Courses = () => {
+  return (
+    <div className="pt-16 pb-12 relative bg-gray-200">
+      {/* Bounce ball */}
+      <Image
+        src="/images/cb.png"
+        alt="image"
+        width={800}
+        height={800}
+        className="absolute top-[30%] animate-bounce"
+      />
+      <div className="w-[80%] pt-8 pb-8 mx-auto">
+        {/* Title */}
+        <h1 className="text-4xl md:text-5xl text-gray-900 font-bold">
+          Popular Courses
+        </h1>
+        {/* Courses */}
+        <div className="md:mt-16 mt-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+          {coursesData.map((course) => {
+            return (
+            // Individual course card
+            <div key={course.id}>
+              <CourseCard course={course}/>
+            </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Courses;
