9440b1c7f2d31f7fc749f02510c8f022ef2770b9
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-12 09:53:58 +0530

    Update CourseCard.tsx

--- components/Home/Course/CourseCard.tsx
+++ components/Home/Course/CourseCard.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+"use client";
 
+import React from 'react'
+import Tilt from "react-parallax-tilt";
+import Image from 'next/image';
+import { FaStar } from 'react-icons/fa';
 
 // define the type
 type Props = {
@@ -15,11 +19,52 @@ type Props = {
         category: string;
     };
 };
-const CourseCard = ({}: Props) => {
+const CourseCard = ({ course }: Props) => {
   return (
-    <div>
-      
-    </div>
+    <Tilt>
+      <div className="bg-white rounded-lg overflow-hidden cursor-pointer">
+        { /* Image */}
+        <div>
+          <Image
+           src={course.image}
+           alt={course.title}
+           width={400}
+           height={400}
+           className="w-full h-full" 
+           
+           />
+        </div>
+        <div className="p-4">
+          {/* Price */}
+          <h1 className="ml-auto relative z-[10] h-20 w-20 flex items-center text-lg font-bold justify-center flex-col mt-[-4rem] rounded-full bg-rose-700 text-white">
+            ${course.price}
+          </h1>
+        {/* information */}
+        <div className="flex items-center mt-6 space-x-4">
+          <span className="text-lg text-black text-opacity-70 font-bold">
+            {course.category}
+            </span>
+            <span className="text-base text-gray-600">{course.author}</span>
+        </div>
+        {/* title */}
+        <h1 className="text-xl text-black font-bold mt-2">{course.title}</h1>
+        {/* reviews */}
+        <div className="flex mt-2 items-center space-x-2">
+          <div className="flex items-center">
+            <FaStar className="w-4 h-4 text-yellow-600" />
+            <FaStar className="w-4 h-4 text-yellow-600" />
+            <FaStar className="w-4 h-4 text-yellow-600" />
+            <FaStar className="w-4 h-4 text-yellow-600" />
+            <FaStar className="w-4 h-4 text-yellow-600" />
+          </div>
+          <span className="text-base text-orange-800 font-semibold">
+            ({course.reviewNumber} Reviews)
+          </span>
+        </div>
+        </div>
+      </div>
+    </Tilt>
+    
   )
 }
 
