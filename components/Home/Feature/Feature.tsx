0fc8aea5119844fee5f3e07727da59209924d677
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-13 21:54:27 +0530

    Create Feature.tsx

---
+++ components/Home/Feature/Feature.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div className="pt-16 pb-16">
+        <div className="mt-8 grid grid-cols-1 xl:grid-cols-2 items-center gap-12 w-[80%] mx-auto">
+            {/* Image */}
+            <div>IMAGE</div>
+            {/* Text content */}
+            <div>TEXT</div>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Feature;
