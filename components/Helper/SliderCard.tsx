7583655acff76dea9c3c79b55045c652d06984ec
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-14 20:42:02 +0530

    Create SliderCard.tsx

---
+++ components/Helper/SliderCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+// define the type
+type Props = {
+    name: string;
+    image: string;
+    role: string;
+};
+
+const SliderCard = ({ image, name, role }: Props) => {
+    return <div>SlideCard</div>;
+
+  
+};
+
+export default SliderCard;
