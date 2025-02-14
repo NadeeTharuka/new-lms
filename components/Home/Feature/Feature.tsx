b5d409ad606bedc2698af35b72e715886537f992
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-14 15:25:20 +0530

    Update Feature.tsx

--- components/Home/Feature/Feature.tsx
+++ components/Home/Feature/Feature.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import Image from 'next/image';
 
 const Feature = () => {
   return (
     <div className="pt-16 pb-16">
         <div className="mt-8 grid grid-cols-1 xl:grid-cols-2 items-center gap-12 w-[80%] mx-auto">
             {/* Image */}
-            <div>IMAGE</div>
+            <Tilt>
+            <div>
+              <Image src="/images/f.png" alt="image" width={1000} height={1000} />
+            </div>
+            </Tilt>
             {/* Text content */}
             <div>TEXT</div>
         </div>
