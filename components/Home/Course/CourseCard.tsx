ff597e5acc693b346ceff7c66141972a3c212e71
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-02-16 22:30:22 +0530

    Update CourseCard.tsx

--- components/Home/Course/CourseCard.tsx
+++ components/Home/Course/CourseCard.tsx
@@ -36,10 +36,7 @@ const CourseCard = ({ course }: Props) => {
            />
         </div>
         <div className="p-4">
-          {/* Price */}
-          <h1 className="ml-auto relative z-[10] h-20 w-20 flex items-center text-lg font-bold justify-center flex-col mt-[-4rem] rounded-full bg-rose-700 text-black">
-            ${course.price}
-          </h1>
+         
         {/* information */}
         <div className="flex items-center mt-6 space-x-4">
           <span className="text-lg text-black text-opacity-70 font-bold">
